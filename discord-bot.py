ba2670e5c2f127e0fbc5e551a18b4ba76ac343e0
Ian Uriegas <ianuriegas@gmail.com>
2023-09-05 15:30:16 -0500

    fix: fixed discord message limit bottleneck.

--- discord-bot.py
+++ discord-bot.py
@@ -1,5 +1,9 @@
 import requests
+import logging
+import datetime
 
+logging.basicConfig(level=logging.DEBUG)  # Set logging level to DEBUG
+logger = logging.getLogger(__name__)
 webhook_url = "https://discord.com/api/webhooks/1145048736398049361/goDZcLfOC1Hk07pfBufmUkAP9s3JBsS158yeqsUFr7mIZMzvHVxmvru3QANmqOvjdMir"
 starter_list = "data/starter_list/starter_list.txt"
 
@@ -7,8 +11,41 @@ starter_list = "data/starter_list/starter_list.txt"
 with open(starter_list, "r") as f:
     file_contents = f.read()
 
-# Send a message with file contents
-payload = {
-    "content": f"**@everyone**\n**Optimal starting lineups for this week:**\n{file_contents}"
-}
-requests.post(webhook_url, json=payload)
+# Split the input text into individual league entries
+entries = file_contents.split("===============================================================================================")
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+
+# Format the date and time as a string
+formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+# Process each entry and send as separate messages
+header_printed = False  # Flag to keep track of whether the header has been printed
+for entry in entries:
+    if entry.strip():
+        if not header_printed:
+            payload = {
+                "content": f"**@everyone**\n**Optimal starting lineups for this week ({formatted_datetime}):**\n" +
+                "==============================================================================================="
+            }
+            try:
+                logger.debug("Sending request to webhook for header...")
+                requests.post(webhook_url, json=payload)
+                logger.debug("Header request sent successfully.")
+            except Exception as e:
+                logger.error("An error occurred while sending header:", exc_info=True)
+            
+            header_printed = True
+        
+        payload = {
+            "content": f"{entry}" +
+            "==============================================================================================="
+        }
+
+        try:
+            logger.debug("Sending request to webhook for entry...")
+            requests.post(webhook_url, json=payload)
+            logger.debug("Entry request sent successfully.")
+        except Exception as e:
+            logger.error("An error occurred while sending entry:", exc_info=True)

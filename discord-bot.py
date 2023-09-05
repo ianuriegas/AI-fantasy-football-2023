e7b232c77d14f3e5c8f5ef8bc25e27bab49327a0
Ian Uriegas <ianuriegas@gmail.com>
2023-09-05 12:28:57 -0500

    feat: changing so the actions workflow runs at *:40 at every single hour. also changed discord from sending the notification until *:00

--- discord-bot.py
+++ discord-bot.py
@@ -1,7 +1,19 @@
+import time
 import requests
 
 webhook_url = "https://discord.com/api/webhooks/1145048736398049361/goDZcLfOC1Hk07pfBufmUkAP9s3JBsS158yeqsUFr7mIZMzvHVxmvru3QANmqOvjdMir"
 starter_list = "data/starter_list/starter_list.txt"
+
+# Function to check if the current time has minutes ending in :00
+def wait_until_end_of_hour():
+    while True:
+        current_minute = time.localtime().tm_min
+        if current_minute == 0:
+            break
+        time.sleep(60)  # Wait for a minute before checking again
+
+# Wait until the end of the hour (when minutes are :00)
+wait_until_end_of_hour()
 
 # Read the contents of the file
 with open(starter_list, "r") as f:
